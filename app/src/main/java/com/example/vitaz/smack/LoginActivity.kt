007ec8f85a8986d4a6d837771c08f449cbf8413d
vit_az <vit.azarov@gmail.com>
2020-07-13 23:16:53 -0300

    created login activity, created intent to rech login activity

---
+++ app/src/main/java/com/example/vitaz/smack/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.vitaz.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginButtonClicked(view: View) {
+
+    }
+
+    fun loginCreateUserButtonClicked(view: View) {
+
+    }
+}
