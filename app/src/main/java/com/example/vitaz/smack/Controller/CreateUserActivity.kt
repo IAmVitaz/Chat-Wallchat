0bb23b3df82120b5a654d7bf353bcbca9a2caaf1
vit_az <vit.azarov@gmail.com>
2020-07-19 16:43:36 -0300

    added spinner function and element to block buttons while API executed on createUserActivity. created toasts to handle errors while executing onClick chain of API requests. adding checks to make sure userName/email/passwords are not empty when button is clicked

--- app/src/main/java/com/example/vitaz/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/vitaz/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.example.vitaz.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.example.vitaz.smack.R
 import com.example.vitaz.smack.Services.AuthService
 import com.example.vitaz.smack.Services.UserDataService
+import com.example.vitaz.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View) {
@@ -54,28 +59,57 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        enableSpinner(true)
 
         val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.registerUser(this, email, password) {registerSuccess ->
+                if (registerSuccess) {
+                    AuthService.loginUser(this, email, password) { loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
+                                if (createSuccess) {
 
-        AuthService.registerUser(this, email, password) {registerSuccess ->
-            if (registerSuccess) {
-                AuthService.loginUser(this, email, password) { loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
-                            if (createSuccess) {
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
-                                finish()
+                                    //create a broadcast to send data to the main activity
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast("Something went wrong, please try again")
+                                }
                             }
+                        } else {
+                            errorToast("Something went wrong, please try again")
                         }
                     }
+                } else {
+                    errorToast("Error during registering new user. Most likely user is already exist. Please try again")
                 }
             }
+        } else {
+            errorToast("Make sure user name, email and password are filled in.")
         }
+    }
 
+    fun errorToast(text: String) {
+        Toast.makeText(this, text,
+                Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner (enable: Boolean) {
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserButton.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorButton.isEnabled = !enable
     }
 }
--- app/src/main/java/com/example/vitaz/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/vitaz/smack/Utilities/Constants.kt
@@ -5,3 +5,6 @@ const val BASE_URl = "https://chatwallchat.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URl}account/register"
 const val URL_LOGIN = "${BASE_URl}account/login"
 const val URL_CREATE_USER = "${BASE_URl}user/add"
+
+//Broadcast constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
