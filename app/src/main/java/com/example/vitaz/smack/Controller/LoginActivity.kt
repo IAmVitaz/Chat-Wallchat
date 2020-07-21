26d725833aa9fbe11e7814c8fc3eabb8f7fd109d
vit_az <vit.azarov@gmail.com>
2020-07-20 22:52:53 -0300

    added findUserByEmail functionin AuthService. LoginActivity changed to use loginUser->findUserByEmail functions

--- app/src/main/java/com/example/vitaz/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/vitaz/smack/Controller/LoginActivity.kt
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.vitaz.smack.R
+import com.example.vitaz.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,23 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginButtonClicked(view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
+
+        AuthService.loginUser(this, email, password) {loginSuccess ->
+            if (loginSuccess) {
+                AuthService.findUserByEmail(this) {findSuccess ->
+                    if (findSuccess) {
+                        //activity is sending broadcast as part of it
+
+                        finish()
+                    }
+
+                }
+            }
+
+        }
+
 
     }
 
--- app/src/main/java/com/example/vitaz/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/vitaz/smack/Utilities/Constants.kt
@@ -5,6 +5,7 @@ const val BASE_URl = "https://chatwallchat.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URl}account/register"
 const val URL_LOGIN = "${BASE_URl}account/login"
 const val URL_CREATE_USER = "${BASE_URl}user/add"
+const val URL_GET_USER = "${BASE_URl}user/byEmail/"
 
 //Broadcast constants
 const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
