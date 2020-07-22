9ec2ba67e2e676afd9a47c3f1ccc59e9857f1d64
vit_az <vit.azarov@gmail.com>
2020-07-21 22:24:51 -0300

    handle errors when fields are empty on LoginActivity, as well as errors for login user functions. added spinner and logic for it. added hideKeyboard function

--- app/src/main/java/com/example/vitaz/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/vitaz/smack/Controller/LoginActivity.kt
@@ -1,39 +1,51 @@
 package com.example.vitaz.smack.Controller
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.input.InputManager
 import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.vitaz.smack.R
 import com.example.vitaz.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_create_user.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginButtonClicked(view: View) {
+        enableSpinner(true)
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
+        hideKeyboard()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) {loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) {findSuccess ->
+                        if (findSuccess) {
+                            //activity is sending broadcast as part of it
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast("Something went wrong, please check email")
+                        }
 
-        AuthService.loginUser(this, email, password) {loginSuccess ->
-            if (loginSuccess) {
-                AuthService.findUserByEmail(this) {findSuccess ->
-                    if (findSuccess) {
-                        //activity is sending broadcast as part of it
-
-                        finish()
                     }
-
+                } else {
+                    errorToast("Something went wrong, please check email/password")
                 }
             }
-
+        } else {
+            errorToast("Please fill in both email and password")
         }
-
-
     }
 
     fun loginCreateUserButtonClicked(view: View) {
@@ -41,6 +53,29 @@ class LoginActivity : AppCompatActivity() {
         startActivity(createUserIntent)
         finish()
 
+    }
 
+    fun errorToast(text: String) {
+        Toast.makeText(this, text,
+                Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner (enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginButton.isEnabled = !enable
+        loginCreateUserButton.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
