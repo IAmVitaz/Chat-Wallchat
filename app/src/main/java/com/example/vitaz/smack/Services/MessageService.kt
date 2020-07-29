1b29bc4cb9d8e976f34229642511fbd684fdc8da
vit_az <vit.azarov@gmail.com>
2020-07-28 23:29:19 -0300

    added new isLoggedIn check for message and channels Emitter.Listeners. added getMessages function in MessageService. called from MainActivity

--- app/src/main/java/com/example/vitaz/smack/Services/MessageService.kt
+++ app/src/main/java/com/example/vitaz/smack/Services/MessageService.kt
@@ -9,6 +9,7 @@ import com.example.vitaz.smack.Controller.App
 import com.example.vitaz.smack.Model.Channel
 import com.example.vitaz.smack.Model.Message
 import com.example.vitaz.smack.Utilities.URL_GET_CHANNELS
+import com.example.vitaz.smack.Utilities.URL_GET_MESSAGES
 import org.json.JSONException
 
 object MessageService {
@@ -51,4 +52,60 @@ object MessageService {
         }
         App.prefs.requestQueue.add(channelsRequest)
     }
+
+    fun getMessages(channelId: String, complete: (Boolean) -> Unit) {
+        val url = "$URL_GET_MESSAGES$channelId"
+        val messagesRequest = object : JsonArrayRequest(Method.GET, url, null, Response.Listener {response ->
+            clearMessages()
+
+            try {
+
+                for (x in 0 until response.length()) {
+                    val message = response.getJSONObject(x)
+                    val id = message.getString("_id")
+                    val messageBody = message.getString("messageBody")
+                    val channelId = message.getString("channelId")
+                    val userName = message.getString("userName")
+                    val userAvatar = message.getString("userAvatar")
+                    val userAvatarColor = message.getString("userAvatarColor")
+                    val timeStamp = message.getString("timeStamp")
+
+                    val newMessage = Message(messageBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    this.messages.add(newMessage)
+                }
+                complete(true)
+
+            } catch (e:JSONException) {
+                Log.d("JSON", "EXT:" + e.localizedMessage)
+                complete(false)
+            }
+
+        }, Response.ErrorListener {
+            Log.d("ERROR", "Could not retrieve messages")
+            complete(false)
+        }) {
+
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+                return headers
+            }
+
+        }
+        App.prefs.requestQueue.add(messagesRequest)
+    }
+
+    fun clearMessages() {
+        messages.clear()
+    }
+
+    fun clearChannels() {
+        channels.clear()
+    }
+
+
 }
--- app/src/main/java/com/example/vitaz/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/vitaz/smack/Utilities/Constants.kt
@@ -8,6 +8,7 @@ const val URL_LOGIN = "${BASE_URl}account/login"
 const val URL_CREATE_USER = "${BASE_URl}user/add"
 const val URL_GET_USER = "${BASE_URl}user/byEmail/"
 const val URL_GET_CHANNELS = "${BASE_URl}channel/"
+const val URL_GET_MESSAGES = "${BASE_URl}message/byChannel/"
 
 //Broadcast constants
 const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
